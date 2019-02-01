b7dc78474f6dc6403bf4215bcdd272c320205b5a
Sean Farber <seanfarber@users.noreply.github.com>
2019-02-01 11:24:26 -0500

    fjdas

--- dbhelper.py
+++ dbhelper.py
@@ -57,17 +57,19 @@ class DBHelper:
     def get_all_crimes(self):
         connection = self.connect()
         try:
-            query = 'SELECT latitude, longitude, date, category, description FROM crimes;'
+            query = "SELECT latitude, longitude, date, category, description FROM crimes;"
             with connection.cursor() as cursor:
                 cursor.execute(query)
-                named_crimes = []
-                for crime in cursor:
-                    named_crime = {'latitude': crime[0],
-                                   'longitude': crime[1],
-                                   'date': datetime.datetime.strftime(crime[2], '%Y- %m-%d'),
-                                   'category': crime[3],
-                                   'description': crime[4]}
-                    named_crimes.append(named_crime)
-                    return named_crimes
+            named_crimes = []
+            for crime in cursor:
+                named_crime = {
+                    'latitude': crime[0],
+                    'longitude': crime[1],
+                    'date': datetime.datetime.strftime(crime[2], '%Y-%m-%d'),
+                    'category': crime[3],
+                    'description': crime[4]
+                }
+                named_crimes.append(named_crime)
+            return named_crimes
         finally:
             connection.close()
