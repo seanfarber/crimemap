ebfffbbe5c9a2736cdeb000471f33bc1cbbcc3cc
Sean Farber <seanfarber@users.noreply.github.com>
2019-01-31 20:52:49 -0500

    jfda

--- dbhelper.py
+++ dbhelper.py
@@ -42,8 +42,8 @@ class DBHelper:
     def add_crime(self, category, date, latitude, longitude, desc):
         connection = self.connect()
         try:
-            query = "INSERT INTO crimes (category, date, latitude, longitude, description) \
-                     VALUES (%s, %s, %s, %s, %s)"
+            query = 'INSERT INTO crimes (category, date, latitude, longitude, description) \
+                     VALUES (%s, %s, %s, %s, %s)'
             with connection.cursor() as cursor:
                 cursor.execute(
                     query, (category, date, latitude, longitude, desc))
