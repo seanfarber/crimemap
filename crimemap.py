bd67dd0a0ab8c84ab38d9f7dcd436a66385021d0
Sean Farber <seanfarber@users.noreply.github.com>
2019-02-15 15:03:34 -0500

    add input validation

--- crimemap.py
+++ crimemap.py
@@ -2,7 +2,10 @@ from flask import Flask
 from flask import render_template
 from flask import request
 import json
+import datetime
+import dateparser
 import dbconfig
+import string
 if dbconfig.test:
     from mockdbhelper import MockDBHelper as DBHelper
 else:
@@ -10,43 +13,45 @@ else:
 
 app = Flask(__name__)
 DB = DBHelper()
+categories = ['mugging', 'break in']
 
 
 @app.route('/')
-def home():
+def home(error_message=None):
     crimes = DB.get_all_crimes()
     crimes = json.dumps(crimes)
-    return render_template('home.html', crimes=crimes)
-
-
-@app.route('/add', methods=['POST'])
-def add():
-    try:
-        data = request.form.get('userinput')
-        DB.add_input(data)
-    except Exception as e:
-        print(e)
-    return home()
-
-
-@app.route('/clear')
-def clear():
-    try:
-        DB.clear_all()
-    except Exception as e:
-        print(e)
-    return home()
+    return render_template('home.html', crimes=crimes, categories=categories, error_message=error_message)
 
 
 @app.route('/submitcrime', methods=['POST'])
 def submitcrime():
     category = request.form.get('category')
-    date = request.form.get('date')
-    latitude = float(request.form.get('latitude'))
-    longitude = float(request.form.get('longitude'))
-    description = request.form.get('description')
+    if category not in categories:
+        return home()
+    date = format_date(request.form.get('date'))
+    if not date:
+        return home('Invalid date.  Please use yyy-mm-dd format')
+    try:
+        latitude = float(request.form.get('latitude'))
+        longitude = float(request.form.get('longitude'))
+    except ValueError:
+        return home()
+    description = sanitize_string(request.form.get('description'))
     DB.add_crime(category, date, latitude, longitude, description)
     return home()
+
+
+def format_date(userdate):
+    date = dateparser.parse(userdate)
+    try:
+        return datetime.datetime.strftime(date, '%Y-%m-%d')
+    except TypeError:
+        return None
+
+
+def sanitize_string(userinput):
+    whitelist = string.ascii_letters + string.digits + " !?$.,;:-'()&"
+    return filter(lambda x: x in whitelist, userinput)
 
 
 if __name__ == '__main__':
--- dbhelper.py
+++ dbhelper.py
@@ -8,38 +8,6 @@ class DBHelper:
     def connect(self, database='crimemap'):
         return pymysql.connect(host='localhost', user=dbconfig.db_user, passwd=dbconfig.db_password, db=database)
 
-    def get_all_inputs(self):
-        connection = self.connect()
-        try:
-            query = 'SELECT description FROM crimes;'
-            with connection.cursor() as cursor:
-                cursor.execute(query)
-            return cursor.fetchall()
-        finally:
-            connection.close()
-
-    def add_input(self, data):
-        connection = self.connect()
-        try:
-            # The following introduces a deliberate security flaw.
-            # query = 'INSERT INTO crimes (description) VALUES ("{}");'.format(data)
-            query = 'INSERT INTO crimes (description) VALUES (%s);'
-            with connection.cursor() as cursor:
-                cursor.execute(query)
-                connection.commit()
-        finally:
-            connection.close()
-
-    def clear_all(self):
-        connection = self.connect()
-        try:
-            query = 'DELETE FROM crimes;'
-            with connection.cursor() as cursor:
-                cursor.execute(query)
-                connection.commit()
-        finally:
-            connection.close()
-
     def add_crime(self, category, date, latitude, longitude, desc):
         connection = self.connect()
         try:
